f36e3c4d00e38fe63a423d3b32e5ae7d4e833809
sungsu <gaecheok@npixel.co.kr>
2020-05-25 14:35:24 +0900

    jpa 용 생성자 추가

--- src/main/java/com/nx/lib/obj/ResponsePagingEnvelop.java
+++ src/main/java/com/nx/lib/obj/ResponsePagingEnvelop.java
@@ -1,5 +1,7 @@
 package com.nx.lib.obj;
 
+import org.springframework.data.domain.Page;
+
 public class ResponsePagingEnvelop<T> {
 	private boolean success;
 
@@ -15,6 +17,19 @@ public class ResponsePagingEnvelop<T> {
 
 	public ResponsePagingEnvelop(boolean success, T content, int size, int page, long totalElements){
 		PageResults<T> pageResults = new PageResults(content, size, page, totalElements);
+		this.result = pageResults;
+		this.success = success;
+	}
+
+	public ResponsePagingEnvelop(boolean success, Page page, String code, String message){
+		PageResults<T> pageResults = new PageResults(page.getContent(), page.getSize(), page.getPageable().getPageNumber(), page.getTotalElements());
+		this.result = pageResults;
+		this.success = success;
+		this.error = new Error(code, message);
+	}
+
+	public ResponsePagingEnvelop(boolean success, Page page){
+		PageResults<T> pageResults = new PageResults(page.getContent(), page.getSize(), page.getPageable().getPageNumber(), page.getTotalElements());
 		this.result = pageResults;
 		this.success = success;
 	}

d034b9bd20a3237f95f0c4227146a51c16dd7eb7
ms.seol <ms.seol@npixel.co.kr>
2020-09-08 19:01:10 +0900

    cbt profile 추가

--- src/main/java/com/nx/lib/bind/annotation/CloseBeta.java
+++ src/main/java/com/nx/lib/bind/annotation/CloseBeta.java
@@ -7,9 +7,9 @@ import java.lang.annotation.Target;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 
-@ConditionalOnExpression("'${spring.profiles.active}' != 'production' and '${spring.profiles.active}' != 'awscbt'")
+@ConditionalOnExpression("'${spring.profiles.active}' == 'awscbt'")
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Developement {
+public @interface CloseBeta {
 
 }

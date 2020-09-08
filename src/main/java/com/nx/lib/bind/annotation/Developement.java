9849cf8a3e391bc012a04f9f6c0119993631dd50
ms.seol <ms.seol@npixel.co.kr>
2020-09-08 19:02:24 +0900

    awscbt -> cbt

--- src/main/java/com/nx/lib/bind/annotation/Developement.java
+++ src/main/java/com/nx/lib/bind/annotation/Developement.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 
-@ConditionalOnExpression("'${spring.profiles.active}' != 'production' and '${spring.profiles.active}' != 'awscbt'")
+@ConditionalOnExpression("'${spring.profiles.active}' != 'production' and '${spring.profiles.active}' != 'cbt'")
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Developement {

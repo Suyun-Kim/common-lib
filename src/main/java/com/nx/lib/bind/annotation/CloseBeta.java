7f9b5647d61824f11f42e6169ef4ce54f8dec18b
ms.seol <ms.seol@npixel.co.kr>
2020-10-08 10:45:52 +0900

    Profile annotation Default conditional 설정

--- src/main/java/com/nx/lib/bind/annotation/CloseBeta.java
+++ src/main/java/com/nx/lib/bind/annotation/CloseBeta.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 
-@ConditionalOnExpression("'${spring.profiles.active}' == 'cbt'")
+@ConditionalOnExpression("'${spring.profiles.active:default}' == 'cbt'")
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CloseBeta {
--- src/main/java/com/nx/lib/bind/annotation/Production.java
+++ src/main/java/com/nx/lib/bind/annotation/Production.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 
-@ConditionalOnExpression("'${spring.profiles.active}' == 'production'")
+@ConditionalOnExpression("'${spring.profiles.active:default}' == 'production'")
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Production {

affc3b73917bd26f7aaf493a0dd1d0385ad87f45
ms.seol <ms.seol@npixel.co.kr>
2022-01-27 15:07:24 +0900

    de

--- src/main/java/com/nx/lib/bind/annotation/localize/Korea.java
+++ src/main/java/com/nx/lib/bind/annotation/localize/Korea.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 
-@ConditionalOnExpression("{'default', 'local','dev', 'tdev', 'ndev', 'qa', 'cbt', 'production', 'crdev', 'crproduction', 'crqa'}.contains('${spring.profiles.active:default}')")
+@ConditionalOnExpression("{'default', 'local', 'gudev', 'dev', 'tdev', 'ndev', 'qa', 'cbt', 'production', 'crdev', 'crproduction', 'crqa'}.contains('${spring.profiles.active:default}')")
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Korea {

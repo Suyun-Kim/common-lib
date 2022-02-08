70afe4021ae400ebe8c5af441d0ab0b90a040833
ms.seol <ms.seol@npixel.co.kr>
2022-02-08 11:50:22 +0900

    revieiw

--- src/main/java/com/nx/lib/bind/annotation/localize/Japan.java
+++ src/main/java/com/nx/lib/bind/annotation/localize/Japan.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 
-@ConditionalOnExpression("{'jlocal','jdev', 'jproduction'}.contains('${spring.profiles.active:default}')")
+@ConditionalOnExpression("{'jlocal','jdev', 'jproduction', 'jreview'}.contains('${spring.profiles.active:default}')")
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Japan {
